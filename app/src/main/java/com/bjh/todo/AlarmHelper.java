e17a8883645b7b483f903890cae4a75c7f8b5264
[배지환] <m9310142@naver.com>
2024-10-29 05:29:47 +0900

    한 시간 전에 알림 기능 완성

--- app/src/main/java/com/bjh/todo/AlarmHelper.java
+++ app/src/main/java/com/bjh/todo/AlarmHelper.java
@@ -1,12 +1,9 @@
 package com.bjh.todo;
 
 import android.app.AlarmManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import java.util.Calendar;
 
